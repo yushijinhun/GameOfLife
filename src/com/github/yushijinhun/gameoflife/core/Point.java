949dca7f4958a279a6d0d6247df66f31b711c851
yushijinhun <yushijinhun@gmail.com>
2014-12-27 14:37:30 +0800

    Update Point.

--- src/com/github/yushijinhun/gameoflife/core/Point.java
+++ src/com/github/yushijinhun/gameoflife/core/Point.java
@@ -6,4 +6,31 @@ public class Point {
 
 	public BigInteger x;
 	public BigInteger y;
+	
+	public Point(BigInteger x, BigInteger y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public int hashCode(){
+		int hash=17;
+		hash=31*hash+x.hashCode();
+		hash=31*hash+y.hashCode();
+		return hash;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if (obj==this) {
+			return true;
+		}
+		
+		if (obj instanceof Point){
+			Point another=(Point) obj;
+			return another.x.equals(x)&&another.y.equals(y);
+		}
+		
+		return false;
+	}
 }
