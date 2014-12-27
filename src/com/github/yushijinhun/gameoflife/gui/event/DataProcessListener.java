e9539580752414d4a7dfd11003b389d4e351e0c5
yushijinhun <yushijinhun@gmail.com>
2014-12-27 15:38:12 +0800

    Delete old files

--- src/com/github/yushijinhun/gameoflife/gui/event/DataProcessListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.yushijinhun.gameoflife.gui.event;
-
-import java.util.EventListener;
-
-public interface DataProcessListener extends EventListener {
-
-	void process(DataProcessEvent e);
-}
